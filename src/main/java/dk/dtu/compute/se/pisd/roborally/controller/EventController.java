cd1c530745da57cb9f7bd33495e19c491091d57f
LuucM <luucmeldgaard@outlook.com>
2023-04-14 16:33:46 +0200

    - Trying to add field events

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/EventController.java
@@ -0,0 +1,53 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+
+import dk.dtu.compute.se.pisd.roborally.model.Board;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+
+public class EventController {
+
+
+    public void moveSpaces(Player player, Board board) {
+        int amount = 2;
+        Space space = player.getSpace();
+        int[] spacePosition = space.getPosition();
+        for (int i = 0; i < amount; i++) {
+            switch (player.getHeading()) {
+                case NORTH -> {
+                    spacePosition[1] -= 2;
+                }
+                case WEST -> {
+                    spacePosition[0] -= 2;
+                }
+                case SOUTH -> {
+                    spacePosition[1] += 2;
+                }
+                case EAST -> {
+                    spacePosition[0] += 2;
+                }
+            }
+        }
+        player.setSpace(board.getSpace(spacePosition[0], spacePosition[1]));
+    }
+
+    public void moveToSpace(Player player) {
+
+    }
+
+    public void turnPlayer(Player player) {
+
+    }
+
+    public void powerDown(Player player) {
+
+    }
+
+    public void repair(Player player) {
+
+    }
+
+    public void fireLaser(Player player, Space space) {
+
+    }
+
+}
