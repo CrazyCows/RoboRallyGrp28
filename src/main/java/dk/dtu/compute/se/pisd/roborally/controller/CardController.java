a7b960c298745cc62af4b82d1889f20c49b10538
Anton <s224268@student.dtu.dk>
2023-04-14 12:07:56 +0200

    We have now decided that all cards not upgrade cards are commandCard(s).

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/CardController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/CardController.java
@@ -1,4 +1,30 @@
 package dk.dtu.compute.se.pisd.roborally.controller;
 
+import dk.dtu.compute.se.pisd.roborally.model.CardLoader;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class CardController {
+    private static CardController cardController;
+    public static CardController getInstance(){ //Singleton
+        if (cardController == null){
+            cardController = new CardController();
+        }
+        return cardController;
+    }
+
+
+
+    public void drawCards(Player player){
+        ArrayList<Object> deck = player.getDeck; //TODO: ADD getDeck and deck
+        player.
+    }
+    private LinkedList<Object> shuffleDeck(LinkedList<Object> deck){
+        Collections.shuffle(deck);
+        return deck;
+    }
+
 }
