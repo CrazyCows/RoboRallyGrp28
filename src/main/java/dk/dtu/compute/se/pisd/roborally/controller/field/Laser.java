8ce35c6b949a6ddb59166f0d041db20fe0b8be45
Anton <113907270+s224268@users.noreply.github.com>
2023-06-02 11:49:10 +0200

    Begun working on Laser

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/field/Laser.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/field/Laser.java
@@ -1,4 +1,34 @@
 package dk.dtu.compute.se.pisd.roborally.controller.field;
 
-public class Laser {
+import dk.dtu.compute.se.pisd.roborally.controller.FieldAction;
+import dk.dtu.compute.se.pisd.roborally.controller.GameController;
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+
+import java.util.ArrayList;
+
+public class Laser extends FieldAction {
+    private Heading heading;
+    public Heading getHeading() {
+        return heading;
+    }
+    public void setHeading(Heading heading) {
+        this.heading = heading;
+    }
+
+    @Override
+    public boolean doAction(GameController gameController, Space space) {
+
+        ArrayList<Space> spaces = space.board.getLaserSpaces();
+        //Space target = gameController.getLineOfSight(space,heading);
+
+/*
+        currentPlayer.addSpamCardToDiscardPile(); //Draws two spam damage cards
+        currentPlayer.addSpamCardToDiscardPile();
+
+        currentPlayer.setHeading(Heading.NORTH);
+
+ */
+        return false;
+    }
 }
