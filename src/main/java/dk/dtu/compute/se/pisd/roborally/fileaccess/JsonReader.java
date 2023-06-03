1f7b6403965e22e2b84652093d64d01687e05a5d
CrazyCows <emil.s.simonsen@gmail.com>
2023-06-03 20:52:48 +0200

    Creating a universal jsonreader with overlaoding...

--- src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/JsonReader.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/JsonReader.java
@@ -1,8 +1,49 @@
 package dk.dtu.compute.se.pisd.roborally.fileaccess;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
 public class JsonReader {
 
     // TODO: something
 
 
+    ObjectMapper objectMapper = new ObjectMapper();
+
+
+    /**
+     * Gets the key value from a jsonFile. The jsonFile must only contain one key value
+     * @param jsonFileName
+     * @param key
+     * @return
+     */
+    public String jsonReadDataInFile(String jsonFileName, String key) {
+
+    }
+
+    /**
+     * This method checks if a value exists in the JSON file and returns it
+     * @Param jsonFile
+     * @Param the value you wish to find
+     * @Param The value you wish to compare to
+     */
+    public String jsonReadDataInFile(String jsonFileName, String key, String valueToCompare) throws IOException{
+        // Read JSON file and convert to list of Person
+        JsonNode node = objectMapper.readTree(new File("data", jsonFileName));
+
+        // Check if the node is an array
+        if (node.isArray()) {
+            // Iterate through array elements
+            for (JsonNode element : node) {
+                // If the valueToCompare field is the key element, print the value is fo
+                if (valueToCompare.equals(element.get(key).asText())) {
+                    return element.get(key).asText();
+                }
+            }
+        }
+        throw new RuntimeException("Data not found in file");
+    }
 }
--- src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/JsonSharedPlayerData.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/JsonSharedPlayerData.java
@@ -13,28 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class JsonSharedPlayerData {
-    ObjectMapper objectMapper = new ObjectMapper();
 
-    /*
-     *** This should be moved to another class. It is currently just to check the json files is OK
-     */
-
-    public void JsonReader() throws IOException{
-        // Read JSON file and convert to list of Person
-        JsonNode node = objectMapper.readTree(new File("data", "collectivePlayerData.json"));
-
-        // Check if the node is an array
-        if (node.isArray()) {
-            // Iterate through array elements
-            for (JsonNode element : node) {
-                // If the "name" field is "John", print the "age" field
-                if ("Player 2".equals(element.get("name").asText())) {
-                    System.out.println("John's age is: " + element.get("color").asText());
-                    break;
-                }
-            }
-        }
-    }
 
     public void getSharedData(){
 
