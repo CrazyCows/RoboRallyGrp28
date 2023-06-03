c7594670c1d0f5a2e91b588a372bb6ebdf3f7e96
LuucM <luucmeldgaard@outlook.com>
2023-06-04 00:04:41 +0200

    - Added new image files for players (made by Haidar) - Made menues for setup of game

--- src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/ClientController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/ClientController.java
@@ -35,139 +35,151 @@ public class ClientController {
 
     }
 
-    public void getJSON(String ID, String jsonName) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(baseUrl + jsonID.get(jsonName) + ID))
-                .GET()
-                .build();
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        if (response.statusCode() != 200) {
-            throw new RuntimeException("Failed : HTTP error code : " + response.statusCode());
-        }
-
-
-        String responseJson = response.body();
-        JsonNode jsonNode = objectMapper.readTree(responseJson);
-        objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(path, jsonName), jsonNode);
-    }
-
-
-    public void createJSON(String ID, String jsonName) throws IOException, InterruptedException {
-        File file = new File(path, jsonName);
-        JsonNode json = objectMapper.readTree(file);
-
-        WebClient webClient = WebClient.create();
-        Mono<String> response = webClient.post()
-                .uri(baseUrl + jsonID.get(jsonName) + ID)
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(json)
-                .retrieve()
-                // Error handling
-                // 409 returns File conflict on server - already exists
-                // 500 returns internal error - Could not write to file
-                .onStatus(httpStatus -> httpStatus.is4xxClientError(), clientResponse ->
-                        clientResponse.bodyToMono(String.class)
-                                .map(body -> {
-                                    JsonNode jsonNode;
-                                    try {
-                                        jsonNode = new ObjectMapper().readTree(body);
-                                        String status = jsonNode.get("status").asText();
-                                        String error = jsonNode.get("error").asText();
-                                        String path = jsonNode.get("path").asText();
-                                        return new RuntimeException("Status: " + status + ", Error: " + error + ", Path: " + path);
-                                    } catch (JsonProcessingException e) {
-                                        e.printStackTrace();
-                                    }
-                                    return null;
-                                }))
-                .onStatus(httpStatus -> httpStatus.is5xxServerError(), clientResponse ->
-                        clientResponse.bodyToMono(String.class)
-                                .map(body -> {
-                                    JsonNode jsonNode;
-                                    try {
-                                        jsonNode = new ObjectMapper().readTree(body);
-                                        String status = jsonNode.get("status").asText();
-                                        String error = jsonNode.get("error").asText();
-                                        String path = jsonNode.get("path").asText();
-                                        return new RuntimeException("Status: " + status + ", Error: " + error + ", Path: " + path);
-                                    } catch (JsonProcessingException e) {
-                                        e.printStackTrace();
-                                    }
-                                    return null;
-                                }))
-                .bodyToMono(String.class);
-
-        String responseString;
+    public boolean getJSON(String ID, String jsonName) {
         try {
-            responseString = response.block();
-            System.out.println("Success");
-            System.out.println(responseString);
-        } catch (RuntimeException e) {
-            System.out.println("Failure");
-            System.out.println(e.getMessage());
-        }
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(baseUrl + jsonID.get(jsonName) + ID))
+                    .GET()
+                    .build();
 
-        /*
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        Mono<String> response = webClient.post()
-                .uri(baseUrl + "/jsonBoards?fileName=" + fileName + "&ID=" + ID)
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(json)
-                .retrieve()
-                .bodyToMono(String.class);
-
-        System.out.println("Success");
-        System.out.println(response.block());
-
-         */
-    }
-
-    public void updateJSON(String ID, String jsonName) throws IOException, InterruptedException{
-        WebClient webClient = WebClient.create();
-        File file = new File(path, jsonName);
-        JsonNode json = objectMapper.readTree(file);
-
-
-        Mono<String> response = webClient.put()
-                .uri(baseUrl + jsonID.get(jsonName) + ID)
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(json)
-                .retrieve()
-                .bodyToMono(String.class);
-
-        String responseString;
-        try {
-            responseString = response.block();
-            System.out.println("Success");
-            System.out.println(responseString);
-        } catch (RuntimeException e) {
-            System.out.println("Failure");
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public void deleteJSON(String ID, String jsonName) throws IOException, InterruptedException {
-        // Create the DELETE request
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(baseUrl + jsonID.get(jsonName) + ID))
-                .DELETE()
-                .build();
-
-
-        try {
-            // Send the request and get the response
-            HttpResponse<Void> response = client.send(request, HttpResponse.BodyHandlers.discarding());
-
-            // Check the response status code
-            if (response.statusCode() == 200) {
-                System.out.println("Success");
-            } else {
-                System.out.println("Fail " + response.statusCode());
+            if (response.statusCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : " + response.statusCode());
             }
-        } catch (Exception e) {
-            System.out.println("An error occurred while sending the request: " + e.getMessage());
+
+
+            String responseJson = response.body();
+            JsonNode jsonNode = objectMapper.readTree(responseJson);
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(path, jsonName), jsonNode);
+            return true;
+        } catch (RuntimeException | IOException | InterruptedException  e) {
+            e.printStackTrace();
+            return false;
         }
     }
+
+
+    public boolean createJSON(String ID, String jsonName) {
+        try {
+            File file = new File(path, jsonName);
+            JsonNode json = objectMapper.readTree(file);
+
+            WebClient webClient = WebClient.create();
+            Mono<String> response = webClient.post()
+                    .uri(baseUrl + jsonID.get(jsonName) + ID)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .bodyValue(json)
+                    .retrieve()
+                    // Error handling
+                    // 409 returns File conflict on server - already exists
+                    // 500 returns internal error - Could not write to file
+                    .onStatus(httpStatus -> httpStatus.is4xxClientError(), clientResponse ->
+                            clientResponse.bodyToMono(String.class)
+                                    .map(body -> {
+                                        JsonNode jsonNode;
+                                        try {
+                                            jsonNode = new ObjectMapper().readTree(body);
+                                            String status = jsonNode.get("status").asText();
+                                            String error = jsonNode.get("error").asText();
+                                            String path = jsonNode.get("path").asText();
+                                            return new RuntimeException("Status: " + status + ", Error: " + error + ", Path: " + path);
+                                        } catch (JsonProcessingException e) {
+                                            e.printStackTrace();
+                                        }
+                                        return null;
+                                    }))
+                    .onStatus(httpStatus -> httpStatus.is5xxServerError(), clientResponse ->
+                            clientResponse.bodyToMono(String.class)
+                                    .map(body -> {
+                                        JsonNode jsonNode;
+                                        try {
+                                            jsonNode = new ObjectMapper().readTree(body);
+                                            String status = jsonNode.get("status").asText();
+                                            String error = jsonNode.get("error").asText();
+                                            String path = jsonNode.get("path").asText();
+                                            return new RuntimeException("Status: " + status + ", Error: " + error + ", Path: " + path);
+                                        } catch (JsonProcessingException e) {
+                                            e.printStackTrace();
+                                        }
+                                        return null;
+                                    }))
+                    .bodyToMono(String.class);
+
+            String responseString;
+            try {
+                responseString = response.block();
+                System.out.println("Success");
+                System.out.println(responseString);
+            } catch (RuntimeException e) {
+                System.out.println("Failure");
+                System.out.println(e.getMessage());
+            }
+        } catch (RuntimeException | IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean updateJSON(String ID, String jsonName) {
+        try {
+            WebClient webClient = WebClient.create();
+            File file = new File(path, jsonName);
+            JsonNode json = objectMapper.readTree(file);
+
+
+            Mono<String> response = webClient.put()
+                    .uri(baseUrl + jsonID.get(jsonName) + ID)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .bodyValue(json)
+                    .retrieve()
+                    .bodyToMono(String.class);
+
+            String responseString;
+            try {
+                responseString = response.block();
+                System.out.println("Success");
+                System.out.println(responseString);
+            } catch (RuntimeException e) {
+                System.out.println("Failure");
+                System.out.println(e.getMessage());
+            }
+        } catch (RuntimeException | IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean deleteJSON(String ID, String jsonName) {
+        try {
+            // Create the DELETE request
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(baseUrl + jsonID.get(jsonName) + ID))
+                    .DELETE()
+                    .build();
+
+
+            try {
+                // Send the request and get the response
+                HttpResponse<Void> response = client.send(request, HttpResponse.BodyHandlers.discarding());
+
+                // Check the response status code
+                if (response.statusCode() == 200) {
+                    System.out.println("Success");
+                } else {
+                    System.out.println("Fail " + response.statusCode());
+                }
+            } catch (Exception e) {
+                System.out.println("An error occurred while sending the request: " + e.getMessage());
+            }
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+
 }
