e7f8a74ee38fcf6f5eccff347f5c5dea4a9bac7b
CrazyCows <emil.s.simonsen@gmail.com>
2023-06-03 21:52:22 +0200

    jsonReader for boardData kinda working

--- src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/JsonReader.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/JsonReader.java
@@ -20,30 +20,74 @@ public class JsonReader {
      * @param key
      * @return
      */
-    public String jsonReadDataInFile(String jsonFileName, String key) {
+    public String getBoardData(String jsonFileName, String key, int x, int y) throws Exception {
+        JsonNode rootNode = objectMapper.readTree(new File("data", jsonFileName));
+        JsonNode spacesNode = rootNode.get("spaces");
 
+        for (JsonNode spaceNode : spacesNode) {
+            if (spaceNode.get("x").asInt() == x && spaceNode.get("y").asInt() == y) {
+                if (spaceNode.has(key)) {
+                    // Assuming that the field is an array and you want the first value
+                    if (spaceNode.get(key).isArray()) {
+                        return spaceNode.get(key).get(0).asText();
+                    }
+                    // If the field is not an array but a simple key-value pair
+                    else {
+                        return spaceNode.get(key).asText();
+                    }
+                }
+            }
+        }
+        for (JsonNode spaceNode : spacesNode) {
+            if (spaceNode.get("x").asInt() == x && spaceNode.get("y").asInt() == y) {
+                if (spaceNode.has("actions")) {
+                    for (JsonNode actionNode : spaceNode.get("actions")) {
+                        if (actionNode.has("INSTANCE") && actionNode.get("INSTANCE").has(key)) {
+                            return actionNode.get("INSTANCE").get(key).asText();
+                        }
+                    }
+                }
+                if (spaceNode.has("items")) {
+                    for (JsonNode itemNode : spaceNode.get("items")) {
+                        if (itemNode.has(key)) {
+                            return itemNode.get(key).asText();
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
     }
 
     /**
-     * This method checks if a value exists in the JSON file and returns it
-     * @Param jsonFile
-     * @Param the value you wish to find
-     * @Param The value you wish to compare to
+     * Retrieves a value related to a player
+     * Might require a NullPointerException
+     * @param jsonFileName Json file name
+     * @param key Json key attribute
+     * @param playerName Player to get value from
+     * @return String value of key
+     * @throws IOException Im lazy :(
      */
-    public String jsonReadDataInFile(String jsonFileName, String key, String valueToCompare) throws IOException{
+    public String getPlayerData(String jsonFileName, String key, String playerName) throws IOException{
         // Read JSON file and convert to list of Person
         JsonNode node = objectMapper.readTree(new File("data", jsonFileName));
 
         // Check if the node is an array
         if (node.isArray()) {
             // Iterate through array elements
+            System.out.println("RUnning");
             for (JsonNode element : node) {
                 // If the valueToCompare field is the key element, print the value is fo
-                if (valueToCompare.equals(element.get(key).asText())) {
+                if (playerName.equals(element.get("name").asText())) {
+                    System.out.println(element.get(key));
                     return element.get(key).asText();
                 }
             }
+        } else if (playerName.equals(node.get("name").asText())) {
+            System.out.println(node.get(key));
+            return node.get(key).asText();
         }
-        throw new RuntimeException("Data not found in file");
+        return "No data";
     }
 }
--- src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/helpRunStatic.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/helpRunStatic.java
@@ -3,6 +3,13 @@ package dk.dtu.compute.se.pisd.roborally.fileaccess;
 public class helpRunStatic {
     public static void main(String[] args) throws Exception{
         ClientController jsonConnect = new ClientController();
-        jsonConnect.createJSON("116", "sharedBoard.json");
+        JsonReader jsonReader = new JsonReader();
+
+        //jsonReader.jsonReadDataInFile("playerData.json", "color", "Player 2");
+
+        System.out.println(
+        jsonReader.getBoardData("sharedBoard.json", "walls", 3, 3));
+
+        //jsonConnect.createJSON("116", "sharedBoard.json");
     }
 }
