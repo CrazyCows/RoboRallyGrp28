9fa975164b1d4a1addd7989ae6425896d5055998
CrazyCows <emil.s.simonsen@gmail.com>
2023-06-04 18:13:51 +0200

    Generalized some jsonReader stuff... Not quite functional but left if someone wants to finish it. It's a waste of time tho hence i stop

--- src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/JsonReader.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/JsonReader.java
@@ -11,6 +11,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class JsonReader {
