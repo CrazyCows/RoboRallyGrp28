64bf4e0e678a4dbd0d8163f89999cf3671d3bf8e
Anton <113907270+s224268@users.noreply.github.com>
2023-06-07 19:58:58 +0200

    Fixed incorrect error print

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
@@ -79,10 +79,6 @@ public class Board extends Subject {
                 spaces[x][y] = space;
             }
         }
-        if (priorityAntennaSpace == null) {
-            System.out.println("ERROR: NO PRIORITY ANTENNA");
-            //TODO: IDK why this error is here. PriorityAntenna still works somehow
-        }
         this.stepMode = false;
         this.timerSecondsCount = 0;
         this.timerIsRunning = false;
@@ -95,12 +91,14 @@ public class Board extends Subject {
                     if (!space.getItems().isEmpty()) {
                         if (space.getItems().get(0).getName().equals("priorityAntenna")) {
                             priorityAntennaSpace = space;
+                            return priorityAntennaSpace;
                         }
                     }
                 }
             }
         }
-        return priorityAntennaSpace;
+        System.out.println("ERROR: NO PRIORITY ANTENNA");
+        return null;
     }
 
     public Integer getGameId() {
