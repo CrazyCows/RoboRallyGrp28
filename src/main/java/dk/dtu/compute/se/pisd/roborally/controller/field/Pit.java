bf4d8e1c176e4061526b097f852f5c32757d7d46
Anton <113907270+s224268@users.noreply.github.com>
2023-06-08 14:59:57 +0200

    Beautified print statement

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/field/Pit.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/field/Pit.java
@@ -22,9 +22,9 @@ public class Pit extends FieldAction { //More generally just used for rebooting 
     //Implement generics instead of overloading?
     public boolean doAction(@NotNull GameController gameController, @NotNull Player currentPlayer) {
         CardController cardController = CardController.getInstance();
-        System.out.println(currentPlayer.getName() + " is now being rebooted");
         cardController.drawSpamCardToDiscardPile(currentPlayer); //Draws two spam damage cards
         cardController.drawSpamCardToDiscardPile(currentPlayer);
+        System.out.println(currentPlayer.getName() + " is now being rebooted");
         currentPlayer.setSpace(currentPlayer.startSpace);
         gameController.getCardController().emptyProgram(currentPlayer);
         currentPlayer.setHeading(Heading.NORTH);
@@ -34,9 +34,9 @@ public class Pit extends FieldAction { //More generally just used for rebooting 
     public boolean doAction(@NotNull GameController gameController, @NotNull Space space) {
         CardController cardController = CardController.getInstance();
         Player currentPlayer = space.getPlayer();
-        System.out.println(currentPlayer.getName() + " is now being rebooted");
         cardController.drawSpamCardToDiscardPile(currentPlayer); //Draws two spam damage cards
         cardController.drawSpamCardToDiscardPile(currentPlayer);
+        System.out.println(currentPlayer.getName() + " is now being rebooted");
         currentPlayer.setSpace(currentPlayer.startSpace);
         gameController.getCardController().emptyProgram(currentPlayer);
         currentPlayer.setHeading(Heading.NORTH);
