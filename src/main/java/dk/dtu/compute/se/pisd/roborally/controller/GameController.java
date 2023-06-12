ced4035887614f2425f21e4155d325d0c1d1c801
Anton <113907270+s224268@users.noreply.github.com>
2023-06-12 22:35:53 +0200

    synchronized my boss

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
@@ -606,7 +606,7 @@ public class GameController {
 
     }
 
-    public void intermediateFunction(){
+    public synchronized void intermediateFunction(){
         stopTimerBeforeTime = true;
         if (online){
             localPlayer.setReady(true);
